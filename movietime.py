9c9edca519360b0c0e1dd8da1b7f143ee74f05a7
brandonnly <brandon@brandonly.me>
2021-05-02 02:49:25 -0400

    fix: typo in author mention

--- movietime.py
+++ movietime.py
@@ -6,5 +6,5 @@ async def tiktok_blur(file_path):
     video = VideoFileClip(file_path)
 
     # render video
-    video.write_videofile('temp/output.mp4',
+    video.write_videofile('temp/output.mp4', preset='ultrafast',
                           ffmpeg_params=['-lavfi', '[0:v]scale=256/81*iw:256/81*ih,boxblur=luma_radius=min(h\,w)/40:luma_power=3:chroma_radius=min(cw\,ch)/40:chroma_power=1[bg];[bg][0:v]overlay=(W-w)/2:(H-h)/2,setsar=1,crop=w=iw*81/256'])
